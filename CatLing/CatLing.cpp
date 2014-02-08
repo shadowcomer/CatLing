f6bb450d60db6a8789df0a3a37e20e9e65275016
Kevin King <nivek_et@hotmail.com>
2014-02-08 20:57:38 +0100

    Creada configuracion inicial del proyecto. Se ha incluido configuracion para las versiones debug y release. De momento se utilizan configuraciones absolutas de mi ordenador. Hay que mejorarlo para poder implementar direcciones relativas al proyecto. El proyecto utiliza el mismo codigo que el modulo de ejemplo dado en BWAPI ExampleAIModule. Esto es solo por motivos de comprobacion de funcionamiento del proyecto creado y de su configuracion.

--- CatLing/CatLing.cpp
+++
@@ -1,6 +0,0 @@
-// CatLing.cpp : Defines the exported functions for the DLL application.
-//
-
-#include "stdafx.h"
-
-
--- CatLing/dllmain.cpp
+++ CatLing/dllmain.cpp
@@ -1,6 +1,12 @@
 // dllmain.cpp : Defines the entry point for the DLL application.
+#define WIN32_LEAN_AND_MEAN
 #include "stdafx.h"
 
+#include <BWAPI.h>
+
+#include "CatlingModule.h"
+
+extern "C" __declspec(dllexport) void gameInit(BWAPI::Game* game) { BWAPI::BroodwarPtr = game; }
 BOOL APIENTRY DllMain( HMODULE hModule,
                        DWORD  ul_reason_for_call,
                        LPVOID lpReserved
@@ -8,12 +14,15 @@ BOOL APIENTRY DllMain( HMODULE hModule,
 {
 	switch (ul_reason_for_call)
 	{
-	case DLL_PROCESS_ATTACH:
-	case DLL_THREAD_ATTACH:
-	case DLL_THREAD_DETACH:
-	case DLL_PROCESS_DETACH:
-		break;
+	case DLL_PROCESS_ATTACH: break;
+	case DLL_THREAD_ATTACH: break;
+	case DLL_THREAD_DETACH: break;
+	case DLL_PROCESS_DETACH: break;
 	}
 	return TRUE;
 }
 
+extern "C" __declspec(dllexport) BWAPI::AIModule* newAIModule()
+{
+  return new CatlingModule();
+}
