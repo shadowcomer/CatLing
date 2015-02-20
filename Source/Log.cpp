c8788984ea067fbb5acf51a4e8501d5e80d2bea8
shadowcomer <kevinks.et@gmail.com>
2015-02-20 12:59:05 +0100

    Fixed indentation

--- Source/Log.cpp
+++ Source/Log.cpp
@@ -14,7 +14,7 @@ de Log
 Log* Log::Instance()
 {
 	if (!m_pInstance){
-		m_pInstance= new Log();
+		m_pInstance = new Log();
 	}
 	return m_pInstance;
 }
@@ -27,19 +27,19 @@ por el parámetro.
 *****************************************************
 */
 int Log::openLogFile(char* file){
-	
-	if(m_fichero==NULL){
-		m_fichero = fopen(file,"w+");
-			if(m_fichero==NULL)
-			{
-			return -2;
-			}
 
-	return 0;
+	if (m_fichero == NULL){
+		m_fichero = fopen(file, "w+");
+		if (m_fichero == NULL)
+		{
+			return -2;
+		}
+
+		return 0;
 	}
 	else
 	{
-	return -1;
+		return -1;
 	}
 
 
@@ -50,21 +50,21 @@ Escribe en el fichero de log el
 string que se pasa por parámetro
 *****************************************************
 */
-void Log::writeToFile(char* entrada) 
+void Log::writeToFile(char* entrada)
 {
-	
+
 	time_t rawtime;
 	struct tm * timeinfo;
 	char buffer[80];
 
-	time (&rawtime);
-	timeinfo=localtime(&rawtime);
-	strftime(buffer,80,"[%I:%M:%S] ",timeinfo);
+	time(&rawtime);
+	timeinfo = localtime(&rawtime);
+	strftime(buffer, 80, "[%I:%M:%S] ", timeinfo);
 
-	strcat(buffer,entrada);
+	strcat(buffer, entrada);
 
-		fwrite(buffer,1,strlen(buffer),m_fichero);
-	
+	fwrite(buffer, 1, strlen(buffer), m_fichero);
+
 }
 /*
 *****************************************************
@@ -73,23 +73,23 @@ string que se pasa por parámetro y ademas
 el nivel de debug
 *****************************************************
 */
-void Log::writeToFile(zas hack,char* entrada) 
+void Log::writeToFile(zas hack, char* entrada)
 {
 	time_t rawtime;
 	struct tm * timeinfo;
 	char buffer[80];
 
-	time (&rawtime);
-	timeinfo=localtime(&rawtime);
-	strftime(buffer,80,"[%I:%M:%S] ",timeinfo);
+	time(&rawtime);
+	timeinfo = localtime(&rawtime);
+	strftime(buffer, 80, "[%I:%M:%S] ", timeinfo);
 
 	char mega[128];
-		itoa(hack,mega,10);
-	char * loginfo=strcat(mega," ");
-	  loginfo=strcat(buffer,mega);
-	  loginfo=strcat(buffer,entrada);
-		fwrite(buffer,1,strlen(buffer),m_fichero);
-	
+	itoa(hack, mega, 10);
+	char * loginfo = strcat(mega, " ");
+	loginfo = strcat(buffer, mega);
+	loginfo = strcat(buffer, entrada);
+	fwrite(buffer, 1, strlen(buffer), m_fichero);
+
 }
 
 /*
--- Source/Log.h
+++ Source/Log.h
@@ -3,7 +3,7 @@
 #include <string.h>
 #include <time.h>
 
-enum zas {DEBUG0,DEBUG1,DEBUG2,DEBUG3};
+enum zas { DEBUG0, DEBUG1, DEBUG2, DEBUG3 };
 #define LOG(x) Log::Instance()->writeToFile( x ) ;
 #define LOGLVL(x,y) Log::Instance()->writeToFile( x , y ) ;
 
@@ -13,7 +13,7 @@ public:
 	static FILE* m_fichero;
 	int openLogFile(char* logfile);
 	void writeToFile(char* entrada);
-	void writeToFile(zas hack,char* entrada) ;
+	void writeToFile(zas hack, char* entrada);
 	int closeLogFile();
 
 private:
