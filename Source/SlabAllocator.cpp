1bab9e2453b5a96af0a67224fbb3682e89da649e
shadowcomer <kevinks.et@gmail.com>
2015-04-28 18:14:57 +0200

    Added traversal and finding functions to the SlabAllocator.

--- Source/SlabAllocator.cpp
+++ Source/SlabAllocator.cpp
@@ -18,6 +18,22 @@ bool SlabAllocator::createSlab(const std::string name, const TypeList& fields)
 }
 
 bool SlabAllocator::destroySlab(const std::string name)
-{
-	return m_slabs.erase(name) == 1;
-}
+	{ return m_slabs.erase(name) == 1; }
+
+auto SlabAllocator::begin()->std::unordered_map<std::string, Slab*>::iterator
+	{ return m_slabs.begin(); }
+
+auto SlabAllocator::begin() const ->std::unordered_map<std::string, Slab*>::const_iterator
+	{ return m_slabs.begin(); }
+
+auto SlabAllocator::end() ->std::unordered_map<std::string, Slab*>::iterator
+	{ return m_slabs.end(); }
+
+auto SlabAllocator::end() const->std::unordered_map<std::string, Slab*>::const_iterator
+	{ return m_slabs.end(); }
+
+auto SlabAllocator::find(std::string slabName)->std::unordered_map<std::string, Slab*>::iterator
+	{ return m_slabs.find(slabName); }
+
+auto SlabAllocator::find(std::string slabName) const->std::unordered_map<std::string, Slab*>::const_iterator
+	{ return m_slabs.find(slabName); }
--- Source/SlabAllocator.h
+++ Source/SlabAllocator.h
@@ -15,6 +15,15 @@ public:
 	bool createSlab(const std::string name, const TypeList& fields);
 	bool destroySlab(const std::string name);
 
+	auto begin()->std::unordered_map<std::string, Slab*>::iterator;
+	auto begin() const ->std::unordered_map<std::string, Slab*>::const_iterator;
+
+	auto end()->std::unordered_map<std::string, Slab*>::iterator;
+	auto end() const->std::unordered_map<std::string, Slab*>::const_iterator;
+
+	auto find(std::string slabName)->std::unordered_map<std::string, Slab*>::iterator;
+	auto find(std::string slabName) const->std::unordered_map<std::string, Slab*>::const_iterator;
+
 private:
 	std::unordered_map<std::string, Slab*> m_slabs;
 };
