99ba5c6b2b33b65592cb75a70c3942c34b34ed3b
shadowcomer <kevinks.et@gmail.com>
2015-05-08 15:09:33 +0200

    Layed out interface for Slab entry appending. Work has to be completed on TypeObj identification first.

--- Source/Slab.cpp
+++ Source/Slab.cpp
@@ -6,5 +6,31 @@ m_fields(fields)
 
 }
 
+Slab::~Slab()
+{
+
+}
+
+auto Slab::canInsert(std::vector<TypeObj*> entry)->bool
+{
+	if (entry.size() != m_entries.size())
+	{
+		return false;
+	}
+
+	for each(auto base in m_entries)
+	{
+
+	}
+
+	return true;
+}
+
+auto Slab::appendEntry()->bool
+{
+
+	return true;
+}
+
 auto Slab::discover()->TypeList const
 	{ return m_fields; }
--- Source/Slab.h
+++ Source/Slab.h
@@ -6,20 +6,24 @@
 
 #include "Types.h"
 
-typedef std::unordered_map<std::string, TypeObj*> TypeList;
+typedef std::unordered_map<std::string, TypeObj const * const> TypeList;
 
 class Slab
 {
 private:
 	const TypeList m_fields;
-	std::vector<TypeObj*> m_entries;
+	std::vector<std::vector<TypeObj*>> m_entries;
 
 public:
 	Slab(const TypeList fields);
 	Slab() = delete;
 	Slab(Slab&) = delete;
 
-	~Slab(){};
+	~Slab();
+
+	auto canInsert(std::vector<TypeObj*> entry)->bool;
+
+	auto appendEntry()->bool;
 
 	auto discover()->TypeList const;
 };
