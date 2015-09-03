6482b5916f7b7fd7e230b40a4607da87a12144b5
shadowcomer <kevinks.et@gmail.com>
2015-09-03 13:31:05 +0200

    Documented the functionality of SlabAllocator's functions.

--- Source/SlabAllocator.h
+++ Source/SlabAllocator.h
@@ -26,15 +26,42 @@ public:
 	SlabAllocator();
 	~SlabAllocator();
 
+	/*
+	Creates a new and empty Slab for this SlabAllocator with the
+	given name.
+	*/
 	bool createSlab(std::string name, TypeList fields);
+
+	/*
+	Destroys the Slab identified by the given name.
+	*/
 	bool destroySlab(const std::string name);
 
+	/*
+	Returns an interator to the beginning of the SlabAllocator's Slabs.
+	*/
 	auto begin()->SlabMap::iterator;
+
+	/*
+	Returns an interator to the beginning of the SlabAllocator's Slabs.
+	*/
 	auto begin() const ->SlabMap::const_iterator;
 
+	/*
+	Returns an interator to the end of the SlabAllocator's Slabs.
+	*/
 	auto end()->decltype(m_slabs.end());
+
+	/*
+	Returns an interator to the end of the SlabAllocator's Slabs.
+	*/
 	auto end() const->SlabMap::const_iterator;
 
+	/*
+	Finds a Slab with the given name.
+	Returns true if found, false otherwise.
+	Fills in 'result' with the Slab, if found.
+	*/
 	auto find(std::string slabName, Slab** result)->bool;
 };
 
