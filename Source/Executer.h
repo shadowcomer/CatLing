24fd9d8b5bc7ae94ca94022f6dc5dfdbc29681a6
shadowcomer <kevinks.et@gmail.com>
2015-09-04 11:54:16 +0200

    Documented this file and it's methods.

--- Source/Executer.h
+++ Source/Executer.h
@@ -1,3 +1,14 @@
+/*
+    The Executer class is responsible for executing the tasks
+    that have been assigned to the queue controlled by the
+    associated TaskManager.
+
+    The Executer should not be built manually, but rather acquired
+    through the call to geOutputInterface of TaskManager. This is
+    because the TaskManager is responsible for the association
+    between its internal queue and the Executer's queue.
+*/
+
 #ifndef EXECUTER_H
 #define EXECUTER_H
 
@@ -7,11 +18,27 @@
 class Executer
 {
 public:
+    /*
+    Do not call this constructor manually. The TaskManager is responsible
+    for building this object, using for its queue parameter its own queue.
+    */
 	Executer(tbb::concurrent_queue<Task*>& queue);
 	~Executer();
 
+    /*
+    Execute a single task from the Executer's queue.
+    */
 	bool executeSingleTask();
+
+    /*
+    Execute every task from the Executer's queue.
+    */
 	int executeAllTasks();
+
+    /*
+    Returns whether the Executer has remaining tasks in the queue
+    waiting for execution.
+    */
 	bool hasRemainingTasks();
 
 private:
