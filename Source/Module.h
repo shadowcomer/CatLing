094fafeffbc7b136ab4c660f614ac80df149bd6d
shadowcomer <kevinks.et@gmail.com>
2015-09-04 10:08:46 +0200

    Documented this file and it's methods.

--- Source/Module.h
+++ Source/Module.h
@@ -1,3 +1,9 @@
+/*
+    This class represents an abstraction over functional modules, allowing for
+    general control of setting up and destruction, as well as access to specific
+    functionality that is shared by all of them.
+*/
+
 #ifndef MODULE_H
 #define MODULE_H
 
@@ -24,14 +30,45 @@ public:
 	Module(Tasker& tsk);
 	~Module();
 
-	// Launches the module and thread
+	/*
+    Launches the module and thread.
+    Each module must implement it's own launching method.
+    As a requirement, m_thread must be set to the thread that is
+    finally in control of the module.
+
+    For example,
+
+        m_thread = tbb::tbb_thread(&MacroManager::run, this);
+
+    is a valid launch.
+    */
 	virtual void launch() = 0;
+
+    /*
+    Tells this module to shutdown.
+    This should only be called by a different thread than the module's.
+    */
 	bool shutdown();
+
+    /*
+    Returns whether this module is in a shutting down state.
+    */
 	bool isTerminating();
+
+    /*
+    Returns the number of frames that should pass inbetween wakes to
+    this module.
+    */
 	int getFramesToWake();
 
+    /*
+    Returns the thread that's in control of this module.
+    */
 	tbb::tbb_thread& getThread();
 
+    /*
+    Sets the SlabAllocator that this module has access to.
+    */
 	bool setAllocator(SlabAllocator* allocator);
 
 protected:
@@ -44,6 +81,10 @@ protected:
 private:
 	int framesToWake;
 	bool m_shuttingDown;
+
+    /*
+    Extra shutdown functionality implemented by each specific module.
+    */
 	virtual bool shutdownHelper() = 0;
 
 };
