4ae88ca56b6b5edf71e67c92cb64d497c88629d9
shadowcomer <kevinks.et@gmail.com>
2015-10-09 16:50:54 +0200

    Added declaration of BT::Node.

---
+++ Source/Node.cpp
@@ -0,0 +1,35 @@
+#include "Node.h"
+
+#include <exception>
+
+using namespace BT;
+
+Node::Node()
+{
+    throw new std::exception("Not implemented");
+}
+
+Node::~Node()
+{
+    throw new std::exception("Not implemented");
+}
+
+void Node::addChild(Node* newChild)
+{
+    throw new std::exception("Not implemented");
+}
+
+void Node::setParent(Node* newParent)
+{
+    throw new std::exception("Not implemented");
+}
+
+Parent Node::parent()
+{
+    throw new std::exception("Not implemented");
+}
+
+Children Node::children()
+{
+    throw new std::exception("Not implemented");
+}
---
+++ Source/Node.h
@@ -0,0 +1,33 @@
+#ifndef NODE_H
+#define NODE_H
+
+#include <memory>
+#include <vector>
+
+namespace BT {
+
+    class Node;
+
+    typedef std::shared_ptr<Node> Parent;
+    typedef std::unique_ptr<Node> Child;
+    typedef std::vector<Child>::iterator Children;
+
+    class Node {
+    public:
+        Node();
+
+        ~Node();
+
+        virtual void addChild(Node* newChild);
+        void setParent(Node* newParent);
+        Parent parent();
+        Children children();
+
+    private:
+        Parent m_parent;
+        std::vector<Child> m_children;
+
+    };
+}
+
+#endif
