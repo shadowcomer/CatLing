e156608613592f9e5cc6810878b4462859ee8e10
shadowcomer <kevinks.et@gmail.com>
2015-10-16 13:26:16 +0200

    Implemented constructor unit test.

--- Test/T_Node.h
+++ Test/T_Node.h
@@ -4,19 +4,26 @@
 #include "Node.h"
 #include "gtest\gtest.h"
 
-namespace {
-    class T_Node : public ::testing::Test
-    {
-    protected:
-        T_Node() {}
-        virtual ~T_Node() {}
-        virtual void SetUp() {}
-        virtual void TearDown() {}
-    };
+class T_Node : public ::testing::Test
+{
+protected:
+    T_Node() {}
+    virtual ~T_Node() {}
+    virtual void SetUp() {}
+    virtual void TearDown() {}
+};
 
-    TEST_F(T_Node, Primer) {
-        EXPECT_EQ(1, 0);
-    }
+TEST(NodeTest, Constructor)
+{
+    // Check parent initialization
+    BT::Node n{};
+    EXPECT_EQ(nullptr, n.parent().get());
+
+    // Check children initialization
+    BT::Children first = n.children();
+    BT::Children end = n.children_end();
+    EXPECT_TRUE(first == end);
 }
+
 
 #endif
