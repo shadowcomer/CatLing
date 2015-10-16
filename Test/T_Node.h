5023c247fabfa5975da2f956bebf06aa2b799dde
shadowcomer <kevinks.et@gmail.com>
2015-10-16 16:10:46 +0200

    Reimplemented tests as fixtures.

--- Test/T_Node.h
+++ Test/T_Node.h
@@ -1,28 +1,48 @@
 #ifndef T_NODE_H
 #define T_NODE_H
 
-#include "Node.h"
 #include "gtest\gtest.h"
 
-class T_Node : public ::testing::Test
+#define private public
+#include "Node.h"
+
+class T_NodeBasic : public ::testing::Test
 {
 protected:
-    T_Node() {}
-    virtual ~T_Node() {}
+    T_NodeBasic():
+        m_node(new BT::Node),
+        m_extra(new BT::Node)
+    {
+
+    }
+
+    virtual ~T_NodeBasic()
+    {
+    }
+
     virtual void SetUp() {}
-    virtual void TearDown() {}
+    virtual void TearDown() { }
+
+    BT::Node* m_node;
+    BT::Node* m_extra;
 };
 
-TEST(NodeTest, Constructor)
+TEST_F(T_NodeBasic, Constructor)
 {
-    // Check parent initialization
-    BT::Node n{};
-    EXPECT_EQ(nullptr, n.parent().get());
+    EXPECT_EQ(nullptr, m_node->m_parent.get());
+    EXPECT_TRUE(m_node->m_children.empty());
+}
 
-    // Check children initialization
-    BT::Children first = n.children();
-    BT::Children end = n.children_end();
-    EXPECT_TRUE(first == end);
+TEST_F(T_NodeBasic, SetNullParent)
+{
+    m_node->setParent(nullptr);
+    EXPECT_EQ(nullptr, m_node->m_parent.get());
+}
+
+TEST_F(T_NodeBasic, SetParent)
+{
+    m_node->setParent(m_extra);
+    ASSERT_EQ(m_node->m_parent.get(), m_extra);
 }
 
 
