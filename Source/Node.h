88fbff702e50e6d9f4e76e6f4978404bb1d2a15e
shadowcomer <kevinks.et@gmail.com>
2015-10-22 11:53:07 +0200

    Added documentation.

--- Source/Node.h
+++ Source/Node.h
@@ -14,14 +14,43 @@ namespace BT {
 
     class Node {
     public:
+        /**
+        Creates a default Node with an empty Parent (nullptr)
+        and no Child nodes.
+        */
         Node();
-
         ~Node();
 
+        /**
+        Adds a new Child to the end of the list of Children.
+
+        newChild: The Node to add to the list of Children.
+        If it's a nullptr, then no action is taken.
+        */
         virtual void addChild(Node* newChild);
+
+        /**
+        Sets a new Parent.
+
+        newParent: The Node to set as the new Parent.
+        If it's a nullptr, then the current Parent is
+        removed.
+        */
         void setParent(Node* newParent);
+
+        /**
+        Retrieves the current Parent of this Node.
+        */
         Parent parent();
+
+        /**
+        Retrieves an iterator to the first Child.
+        */
         Children children();
+
+        /**
+        Retrieves an iterator to the end of the list of Children.
+        */
         Children children_end();
 
     protected:
