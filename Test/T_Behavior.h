ef13724abae7fa56c88543f23be9b05df3144347
shadowcomer <kevinks.et@gmail.com>
2015-10-30 12:49:03 +0100

    Partial implementation of 'iterate' test.

--- Test/T_Behavior.h
+++ Test/T_Behavior.h
@@ -15,6 +15,12 @@ public:
     // of the tested class.
     // #####################################
 
+    MockBehavior() :
+        enter_count(0),
+        iter_count(0),
+        exit_count(0)
+    {};
+
     BT::Parent* t_parent();
     vector<BT::Child>* t_children();
 
@@ -23,6 +29,22 @@ public:
     // #####################################
     void notify(BT::Parent const & who, BT::State newState) { 3 + 3; }
     BT::State doIterate() { return BT::State::SUCCESS; }
+
+    void incEnter(BT::State) {
+        enter_count++;
+    }
+
+    void incExit(BT::State) {
+        exit_count++;
+    }
+
+    void incIter(BT::State) {
+        iter_count++;
+    }
+
+    int enter_count;
+    int exit_count;
+    int iter_count;
 
 protected:
     
@@ -95,6 +117,35 @@ TEST_F(T_BehaviorBasic, SetParent)
     // CHECK
     current_setValue = parentVar->get();
     EXPECT_EQ(expected_clear.get(), parentVar->get());
+}
+
+TEST_F(T_BehaviorBasic, Iterate)
+{
+    using namespace std::placeholders;
+    std::shared_ptr<MockBehavior> behavior =
+        make_shared<MockBehavior>();
+
+    BT::EnterHook enter_h =
+        std::bind(&MockBehavior::incEnter, behavior, _1);
+    BT::IterateHook iterate_h =
+        std::bind(&MockBehavior::incIter, behavior, _1);
+    BT::ExitHook exit_h =
+        std::bind(&MockBehavior::incExit, behavior, _1);
+
+    behavior->setEnterHook(enter_h);
+    behavior->setIterationHook(iterate_h);
+    behavior->setExitHook(exit_h);
+
+    ASSERT_EQ(0, behavior->enter_count);
+    ASSERT_EQ(0, behavior->iter_count);
+    ASSERT_EQ(0, behavior->exit_count);
+
+    behavior->iterate();
+
+    EXPECT_EQ(1, behavior->enter_count);
+    EXPECT_EQ(1, behavior->iter_count);
+    EXPECT_EQ(1, behavior->exit_count);
+
 }
 
 #endif
