137513d27be1eced669f7677f1a058c5a79340e9
shadowcomer <kevinks.et@gmail.com>
2015-11-04 13:29:54 +0100

    Implemented Sequence layout.

--- Source/Sequence.cpp
+++ Source/Sequence.cpp
@@ -1,1 +1,25 @@
 #include "Sequence.h"
+
+using namespace BT;
+
+Sequence::Sequence():
+m_currentChildOffset(0)
+{
+
+}
+
+Sequence::~Sequence()
+{
+
+}
+
+State Sequence::doIterate()
+{
+    return State::SUCCESS;
+}
+
+void Sequence::treatNotification(Behavior const * const who,
+    State oldState)
+{
+
+}
--- Source/Sequence.h
+++ Source/Sequence.h
@@ -7,9 +7,21 @@ namespace BT {
     class Sequence : public Composite
     {
     public:
+        Sequence();
+        ~Sequence();
 
     protected:
+        virtual State doIterate();
 
+    private:
+        unsigned int m_currentChildOffset;
+
+        /**
+        Implementation of what to do when the Behavior
+        implementation has been notified through 'notifyParent'.
+        */
+        virtual void treatNotification(Behavior const * const who,
+            State oldState);
     };
 };
 
