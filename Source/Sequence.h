d59c685dee2283bf2a94ea04f684b2aad1f32e56
shadowcomer <kevinks.et@gmail.com>
2015-11-04 13:01:35 +0100

    Created Sequence header and source.

---
+++ Source/Sequence.h
@@ -0,0 +1,16 @@
+#ifndef SEQUENCE_H
+#define SEQUENCE_H
+
+#include "Composite.h"
+
+namespace BT {
+    class Sequence : public Composite
+    {
+    public:
+
+    protected:
+
+    };
+};
+
+#endif
