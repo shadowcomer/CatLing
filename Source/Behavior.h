233042509fca9368ae476990715df25626d859c8
shadowcomer <kevinks.et@gmail.com>
2015-12-03 16:37:18 +0100

    Add documentation for Behavior

--- Source/Behavior.h
+++ Source/Behavior.h
@@ -21,13 +21,31 @@ namespace bt {
 
     class Behavior {
     public:
+/*
+A Behavior that does not have a parent is a root Behavior.
+If monitorization is not required, the given BehaviorMonitor
+should have an empty implementation.
+*/
         Behavior(Behavior * parent, BehaviorMonitor monitor);
         virtual ~Behavior();
 
+/*
+A default constructor does not give any value to the constructed
+Behavior.
+*/
         Behavior() = delete;
 
         State currentState() const;
+
+/*
+Interface for iteration over Behaviors. Each implementation of a
+Behavior must determine which Behavior is next.
+*/
         virtual Behavior * nextBehavior() = 0;
+
+/*
+Interface for invocation of a single iteration over this Behavior.
+*/
         virtual void tick() = 0;
 
     private:
