e2e4abd4cc5a12747607a609fb3412e5ac18f88e
shadowcomer <kevinks.et@gmail.com>
2016-01-27 16:01:55 +0100

    Move measurement code to its own file
    (cherry picked from commit fbd54322cad0f44cc7d8d491225a12314451c537)

--- Source/Commander.cpp
+++ Source/Commander.cpp
@@ -3,31 +3,13 @@
 /*
 BT Test headers
 */
-#include <chrono>
+#include "Measure.h"
 
 #include "BehaviorTree.h"
 #include "ActionBehavior.h"
 #include "Sequence.h"
 /*
 BT Test headers
-*/
-
-/*
-BT Measuring
-*/
-template<typename TimeT = std::chrono::milliseconds>
-struct Measure {
-    template<typename F, typename ...Args>
-    static typename TimeT::rep execution(F func, Args&&... args) {
-        auto start = std::chrono::system_clock::now();
-        func(std::forward<Args>(args)...);
-        auto duration = std::chrono::duration_cast<TimeT>(
-            std::chrono::system_clock::now() - start);
-        return duration.count();
-    }
-};
-/*
-BT Measuring end
 */
 
 /*
