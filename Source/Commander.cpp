9b7a17fdb50df6a97afab3a26377019ceb3e1ee5
shadowcomer <kevinks.et@gmail.com>
2016-02-02 09:46:34 +0100

    Complete gather minerals implementation

--- Source/Commander.cpp
+++ Source/Commander.cpp
@@ -26,10 +26,45 @@ bool Commander::shutdownHelper()
 }
 
 std::unique_ptr<bt::BehaviorTree> Commander::buildGatherMinerals() {
+    Slab* workers;
+    if (!m_allocator->find("workers", &workers)){
+        std::cout << "Couldn't find the 'workers' slab." << std::endl;
+        return nullptr;
+    }
 
+    std::unique_ptr<bt::Behavior> sendRetrieveMineralsB =
+        std::make_unique<bt::ActionBehavior>(
+        nullptr,
+        [](bt::Behavior* b) { std::cout << "In TAllGatherMinerals" <<
+        std::endl; },
+        std::make_unique<TAllGatherMinerals>(workers));
+
+    std::unique_ptr<bt::Behavior> getWorkersB =
+        std::make_unique<bt::ActionBehavior>(
+        nullptr,
+        [](bt::Behavior* b) { std::cout << "In TRetrieveWorkers" <<
+        std::endl; },
+        std::make_unique<TRetrieveWorkers>(workers));
+
+    std::vector<bt::Behavior*> childrenBehaviors{ getWorkersB.get(),
+        sendRetrieveMineralsB.get() };
+
+    std::unique_ptr<bt::Behavior> seq = std::make_unique<bt::Sequence>
+        (nullptr,
+        [](bt::Behavior* b) { std::cout << "In Sequence" <<
+        std::endl; },
+        childrenBehaviors);
+
+    getWorkersB->setParent(seq.get());
+    sendRetrieveMineralsB->setParent(seq.get());
 
     bt::BehaviorList behaviors;
-    return std::make_unique<bt::BehaviorTree>(std::move(behaviors));
+    behaviors.push_back(std::move(seq));
+    behaviors.push_back(std::move(getWorkersB));
+    behaviors.push_back(std::move(sendRetrieveMineralsB));
+
+    return std::move(
+        std::make_unique<bt::BehaviorTree>(std::move(behaviors)));
 }
 
 void Commander::run(Commander* m)
