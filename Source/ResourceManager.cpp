51ea342a19e4e0147deeea4644145863c887e092
shadowcomer <kevinks.et@gmail.com>
2016-04-01 16:29:50 +0200

    Implement requestResources
    This is a dummy implementation, and will not consider any units requested.
    Even when units are requested, the request will pass if all other conditions are
    passed.

--- Source/ResourceManager.cpp
+++ Source/ResourceManager.cpp
@@ -12,9 +12,32 @@ ResourceManager::~ResourceManager() {
 
 ResourceRequestAnswer ResourceManager::requestResources(
     ResourceRequest const res) {
-    ResourceRequestAnswer answer;
+    ResourceRequestAnswer answer{res};
 
+    // Start as always conceded, so we can easily negate it when it
+    // fails in any of the requested resources
+    answer.conceded = true;
 
+    // Concede minerals
+    if (m_virtualMinerals < res.minerals) {
+        answer.missingMinerals = res.minerals - m_virtualMinerals;
+        answer.conceded = false;
+    }
+
+    // Concede gas
+    if (m_virtualGas < res.minerals) {
+        answer.missingGas = res.gas - m_virtualGas;
+        answer.conceded = false;
+    }
+
+    // Concede units
+    // Dummy: check nothing, concede nothing, accept request.
+
+    // Commit resources
+    if (answer.conceded) {
+        m_virtualMinerals -= res.minerals;
+        m_virtualGas -= res.gas;
+    }
 
     return answer;
 }
