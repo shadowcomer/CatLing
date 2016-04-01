d302cc622c197929e4cf2367ed66afcbfcef76e3
shadowcomer <kevinks.et@gmail.com>
2016-04-01 16:15:45 +0200

    Implement ResourceRequestAnswer

--- Source/ResourceRequestAnswer.cpp
+++ Source/ResourceRequestAnswer.cpp
@@ -1,7 +1,13 @@
 #include "ResourceRequestAnswer.h"
 
 
-ResourceRequestAnswer::ResourceRequestAnswer() {
+ResourceRequestAnswer::ResourceRequestAnswer
+    (ResourceRequest original):
+    originalRequest(original),
+    conceded(false),
+    missingMinerals(0),
+    missingGas(0),
+    missingUnits(std::vector<UnitResource>()) {
 
 }
 
--- Source/ResourceRequestAnswer.h
+++ Source/ResourceRequestAnswer.h
@@ -1,11 +1,23 @@
 #ifndef RESOURCEREQUESTANSWER_H
 #define RESOURCEREQUESTANSWER_H
 
+#include <vector>
+#include "ResourceRequest.h"
+
 class ResourceRequestAnswer
 {
 public:
-    ResourceRequestAnswer();
+    ResourceRequestAnswer(ResourceRequest original);
     ~ResourceRequestAnswer();
+
+    ResourceRequest originalRequest;
+
+    bool conceded;
+
+    unsigned int missingMinerals;
+    unsigned int missingGas;
+
+    std::vector<UnitResource> missingUnits;
 };
 
 #endif
