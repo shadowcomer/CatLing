ada722b8b55257d88f71858fcde4d4f00792c4e2
shadowcomer <kevinks.et@gmail.com>
2016-04-12 18:56:41 +0200

    Implement barracks plan creation

--- Source/MacroPlanner.cpp
+++ Source/MacroPlanner.cpp
@@ -32,21 +32,65 @@ std::unique_ptr<bt::BehaviorTree>
 
 std::unique_ptr <bt::BehaviorTree> MacroPlanner::planBarracks() {
     Slab* workers = nullptr;
+    Slab* builders = nullptr;
 
     if (!m_allocator->find("workers", &workers)) {
-        std::cout << "Couldn't find the 'workers' slab." << std::endl;
+        std::cout << "Couldn't find the 'workers' slab." <<
+            std::endl;
         return nullptr;
     }
+
+    if (!m_allocator->find("builders", &builders)) {
+        std::cout << "Couldn't find the 'builders' slab." <<
+            std::endl;
+        return nullptr;
+    }
+
+    //
+    // Builder Selection
+    //
+    std::unique_ptr<bt::Behavior> selectBuilderB =
+        std::make_unique<bt::ActionBehavior>(
+        nullptr,
+        [](bt::Behavior* b) {},
+        std::make_unique<TaskWrapper>(
+        std::make_unique<TSelectBuilder>(
+        workers, builders)));
+
+
+    //
+    // Resource Request
+    //
+
+    // WARNING: Does not ask for required unit. That is not
+    // implemented yet.
+    ResourceRequest requiredResources;
+    requiredResources.minerals =
+        BWAPI::UnitTypes::Terran_Barracks.mineralPrice();
+    requiredResources.gas =
+        BWAPI::UnitTypes::Terran_Barracks.gasPrice();
+
+    std::unique_ptr<bt::Behavior> resourcesB =
+        std::make_unique<bt::ActionBehavior>(
+        nullptr,
+        [](bt::Behavior* b) {},
+        std::make_unique<TRequestResources>(
+        requiredResources,
+        m_resourceSource));
+
+    //
+    // Building Construction
+    //
 
     UnitTypeFun buildingFun = []() -> BWAPI::UnitType {
         return BWAPI::UnitTypes::Terran_Barracks;
     };
 
-    UnitFun builderFun = [workers]() -> BWAPI::Unit {
-        Entry workerE;
-        bool acquired = workers->getAndRemoveEntry(0, workerE);
+    UnitFun builderFun = [builders]() -> BWAPI::Unit {
+        Entry builderE;
+        bool acquired = builders->getAndRemoveEntry(0, builderE);
 
-        return acquired ? workerE[0]->toUnit()->value :
+        return acquired ? builderE[0]->toUnit()->value :
             nullptr;
     };
 
@@ -67,7 +111,32 @@ std::unique_ptr <bt::BehaviorTree> MacroPlanner::planBarracks() {
         buildingFun,
         locationFun)));
 
+    //
+    // Sequence: Select -> Request -> Build
+    //
+
+    std::vector<bt::Behavior*> childrenBehaviors{
+        selectBuilderB.get(),
+        resourcesB.get(),
+        buildBarracksB.get() };
+
+    std::unique_ptr<bt::Behavior> seq = std::make_unique<bt::Sequence>
+        (nullptr,
+        [](bt::Behavior* b) {},
+        childrenBehaviors);
+
+    selectBuilderB->setParent(seq.get());
+    resourcesB->setParent(seq.get());
+    buildBarracksB->setParent(seq.get());
+
+    //
+    // Tree
+    //
+
     bt::BehaviorList behaviors;
+    behaviors.push_back(std::move(seq));
+    behaviors.push_back(std::move(selectBuilderB));
+    behaviors.push_back(std::move(resourcesB));
     behaviors.push_back(std::move(buildBarracksB));
 
     return std::move(
